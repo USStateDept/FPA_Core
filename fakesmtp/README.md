09fcf90bc8a4f7656de130ba7fa26f9ec3a8b778
nathanhilbert <nathanhilbert@gmail.com>
2015-03-31 13:07:45 -0500

    add in fake stmp files directly

---
+++ fakesmtp/README.md
@@ -0,0 +1,3 @@
+# Fake SMTP
+Print all emails to a file and the std output
+python stmp_devel.py
---
+++ fakesmtp/fake_smtp.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 29 09:32:45 2012
+
+@author: hilbert.34
+"""
+
+import smtpd
+import asyncore
+
+class FakeSMTPServer(smtpd.SMTPServer):
+    """A Fake smtp server"""
+
+    def __init__(*args, **kwargs):
+        print "Running fake smtp server on port 25"
+        smtpd.SMTPServer.__init__(*args, **kwargs)
+
+    def process_message(*args, **kwargs):
+        fromaddress = args[2]
+        messagebody = args[4]
+        
+        try:
+            f = open('c:/inetpub/smtpmail/log/emaillog.log', 'a')
+        except:
+            f = open('c:/inetpub/smtpmail/log/emaillog.log', 'a')
+        f.write("/************************************/\n")
+        f.write(fromaddress + "\n")
+        f.write(messagebody + "\n")
+        pass
+
+if __name__ == "__main__":
+    smtp_server = FakeSMTPServer(('localhost', 25), None)
+    try:
+        asyncore.loop()
+    except KeyboardInterrupt:
+        smtp_server.close()
