ff20337ff52011ef9b52a2dfecf18a32d2c71be5
nathanhilbert <nathanhilbert@gmail.com>
2015-04-15 08:26:10 -0500

    added in time tble for geometry

---
+++ fixtures/timescript/README.md
@@ -0,0 +1,1 @@
+Use this to take a list of countries and add a new time column for the year ranges
---
+++ fixtures/timescript/addtime_countrygroupings.py
@@ -0,0 +1,33 @@
+
+
+import dataset
+
+db = dataset.connect('****INSERT URI******')
+
+geomtable = db['geometry__country_level0']
+
+
+yearrange = range(1960,2050)
+
+timetable = db['geometry__time']
+
+import sys
+
+
+count = 1
+for row in geomtable.all():
+    print "starting", row.get('label')
+    gid = row.get('gid', None)
+    if not gid:
+        print "BROKEN!"
+        sys.exit(1)
+    #gid
+    for year in yearrange:
+        timetable.upsert(dict(id=count, year=year, gid=gid), ['year', 'gid'])
+        count += 1
+
+
+# table.insert(dict(name='John Doe', age=37))
+# table.insert(dict(name='Jane Doe', age=34, gender='female'))
+
+# john = table.find_one(name='John Doe')
