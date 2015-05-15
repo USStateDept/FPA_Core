c76ed6d562072b30b13f21849e72be54d333cced
nathanhilbert <nathanhilbert@gmail.com>
2015-05-15 13:16:46 -0500

    new user object that does not allow admin rights

--- openspending/views/home.py
+++ openspending/views/home.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, flash
 from flask.ext.login import current_user, login_user, logout_user
 
+from openspending.core import login_manager
+
 from openspending.model.dataset import Dataset
 from openspending.views.cache import disable_cache
 from openspending.model import Account
@@ -10,22 +12,40 @@ from settings import LOCKDOWNUSER, LOCKDOWNPASSWORD
 
 blueprint = Blueprint('home', __name__)
 
+
+class User():
+    def is_authenticated(self):
+        return True
+    def is_active(self):
+        return True
+    def is_anonymous(self):
+        return False
+    def get_id(self):
+        return 'all'
+    @property
+    def admin(self):
+        False
+
+@login_manager.user_loader
+def load_user(userid):
+    return User()
+
+
+
 @blueprint.route('/lockdown')
 def lockdown():
     return render_template('home/lockdown.html')
+
 
 
 @blueprint.route('/lockdown', methods=['POST', 'PUT'])
 def lockdown_perform():
     username = request.form.get('username', '')
     password = request.form.get('password', '')
-    print username
-    print password
-    print LOCKDOWNUSER
-    print LOCKDOWNPASSWORD
+
     if username.lower() == LOCKDOWNUSER and password == LOCKDOWNPASSWORD:
-        account = Account.all().first()
-        print account
+        account = load_user('all')
+        #account = Account.all().first()
         login_user(account, remember=True)
         return redirect("/", code=302)
     else:
