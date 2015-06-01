920f98f76ac88d622e0b42754ebb60055041b9f1
nathanhilbert <nathanhilbert@gmail.com>
2015-06-01 11:29:12 -0500

    remove unused file

--- openspending/views/api_v2/__init__.py
+++ openspending/views/api_v2/__init__.py
@@ -1,5 +1,4 @@
 #from openspending.views.api_v2.search import search # noqa
-from openspending.views.api_v2.new import create # noqa
 from openspending.views.api_v2.cubes_ext import aggregate_cubes, cubes_model, cubes_facts
 
 from openspending.views.api_v2.common import blueprint # noqa
