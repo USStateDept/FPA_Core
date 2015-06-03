761a7ed16544857f2bf6a6f037bc6cbe9a560808
nathanhilbert <nathanhilbert@gmail.com>
2015-06-03 09:59:33 -0500

    clean up functions in viz

--- openspending/views/viz.py
+++ openspending/views/viz.py
@@ -3,7 +3,6 @@ import logging
 from StringIO import StringIO
 from urllib import urlencode
 
-from webhelpers.feedgenerator import Rss201rev2Feed
 from werkzeug.exceptions import BadRequest
 from flask import Blueprint, render_template, request, redirect
 from flask import Response
@@ -11,11 +10,6 @@ from flask.ext.login import current_user
 
 from openspending.core import db
 from openspending.model import Tags
-from openspending.lib.csvexport import write_csv
-from openspending.lib.jsonexport import jsonify
-from openspending.lib.indices import cached_index
-from openspending.reference.country import COUNTRIES
-from openspending.views.cache import etag_cache_keygen, disable_cache
 
 log = logging.getLogger(__name__)
 
