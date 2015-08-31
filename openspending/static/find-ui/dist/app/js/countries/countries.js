9511492bb882a2811abe3622b1ed2a64e5d80aa0
Boris Shporkin <boris.shporkin@gmail.com>
2015-08-31 14:31:18 -0700

    working on styling changes and auto select for countries; eod commit

--- openspending/static/find-ui/dist/app/js/countries/countries.js
+++ openspending/static/find-ui/dist/app/js/countries/countries.js
@@ -4,19 +4,26 @@
         window.visualization.createMap();
     }, 10);
 	
-	$("#filterCountries").keyup(function(){
-		
-		var filterValue = this.value;
-
-		var countries = vizModel.countriesModelMaster();
-		vizModel.countriesModel.removeAll();
-
-		for (var x in countries) {
-			if (countries[x].label.toLowerCase().indexOf(filterValue.toLowerCase()) >= 0) {
-				vizModel.countriesModel.push(countries[x]);
-			}
+	$("#filterCountries").keyup(function(e){
+		if (e.keyCode==13){
+			$("#countryList div div").first().click();
 		}
+		else{
 
-		return true;
+			var filterValue = this.value;
+
+			var countries = vizModel.countriesModelMaster();
+			vizModel.countriesModel.removeAll();
+
+			for (var x in countries) {
+				if (countries[x].label.toLowerCase().indexOf(filterValue.toLowerCase()) >= 0) {
+					vizModel.countriesModel.push(countries[x]);
+				}
+			}
+
+			$("#countryList div").first().css("background-color","#FFFF00");
+
+			return true;
+		}
     });
 }())
