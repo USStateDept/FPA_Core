76caad6708ebaeafa94bc5b9427e8a3046dcd709
Michael Ramos <mdramos@radford.edu>
2015-09-09 05:30:05 -0700

    testing js related to issue #303

--- openspending/static/find-ui/dist/app/js/tests/test.js
+++ openspending/static/find-ui/dist/app/js/tests/test.js
@@ -92,6 +92,42 @@ var TESTSERVER = {
       console.log("call 2.2 complete");
     });
 
+  },
+
+  // peform test 1 in callback form
+  test3: function() {
+
+    // call 1
+    $.ajax({
+        url: "/api/slicer/cube/geometry/cubes_aggregate?&cluster=jenks&numclusters=4&cubes=number_of_people_affected_by_n&cut=geometry__time:1990-2015&order=time&drilldown=geometry__time",
+        dataType: "json",
+        data: {}
+    }).done(function( data ) {
+      console.log("call 1 success");
+    }).fail(function( jqXHR, textStatus, errorThrown ) {
+      console.log("call 1 failed");
+    }).always(function( a, textStatus ){
+      console.log("call 1 complete");
+    }).then(function() {
+      // call 2
+      $.ajax({
+          url: "/api/slicer/cube/geometry/cubes_aggregate?&cluster=jenks&numclusters=4&cubes=number_of_people_affected_by_n&cut=geometry__time:1990-2015&order=time&drilldown=geometry__country_level0@dos_region|geometry__time",
+          dataType: "json",
+          data: {}
+      }).done(function( data ) {
+        console.log("call 2 success");
+      }).fail(function( jqXHR, textStatus, errorThrown ) {
+        console.log("call 2 failed");
+      }).always(function( a, textStatus ){
+        console.log("call 2 complete");
+      });
+    })
+
+
+
+
+
+
   }
 
 
