3729af87f4821e3073edcddc3667ce0fe7c2eaa8
Michael Ramos <mdramos@radford.edu>
2015-09-09 05:16:51 -0700

    testing js related to issue #303

--- openspending/static/find-ui/dist/app/js/common/loader.js
+++ openspending/static/find-ui/dist/app/js/common/loader.js
@@ -202,27 +202,21 @@
         //debugger;
         var defferreds = [];
 
+
         _.forEach(urls, function(item) {
+          console.log("URL: " + item.url);
             var d = $.ajax({
                 url: item.url,
                 dataType: "json",
-                data: {
-
-                }
+                data: {}
             }).done(function( data ) {
               // success
               console.log("SUCCESS data: " + data);
             }).fail(function( jqXHR, textStatus, errorThrown ) {
               // failure
-              console.log("FAILURE textStatus: " + textStatus);
               console.log("FAILURE errorThrown: " + errorThrown);
-
-              //fail and send whatever contents we have thus failure
-              return defferreds;
-              
             }).always(function( a, textStatus ){
               // complete no matter what
-              console.log("COMPLETE data: " + a);
               console.log("COMPLETE textStatus: " + textStatus);
             });
 
